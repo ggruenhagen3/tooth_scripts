f9bdc480e231608779269a5e18854ed7a4b64973
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-10 16:09:50 -0400

    Cytotrace

--- cytotrace.R
+++ cytotrace.R
@@ -10,7 +10,7 @@ combined <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/combined.Rds")
 nrow(combined@reductions$umap@cell.embeddings[,1:2])
 ncol(as.matrix(combined@assays$RNA@counts))
 
-non_empty <- combined@assays$RNA@counts[which(rowSums(combined@assays$RNA@counts != 0)),]
+non_empty <- combined@assays$RNA@counts[which(rowSums(combined@assays$RNA@counts) != 0),]
 nrow(non_empty)
 
 results <- CytoTRACE(as.matrix(combined@assays$RNA@counts))
