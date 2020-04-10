53e2d16c64421d2bfff206f235f931891d32d890
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-10 16:28:33 -0400

    Cytotrace

--- cytotrace.R
+++ cytotrace.R
@@ -5,13 +5,10 @@ library("cowplot")
 library("CytoTRACE")
 
 combined <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/combined.Rds")
+epi      <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/epi.Rds")
+jpool    <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/jpool.Rds")
+tj       <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/tj.Rds")
 
-
-nrow(combined@reductions$umap@cell.embeddings[,1:2])
-ncol(as.matrix(combined@assays$RNA@counts))
-
-non_empty <- combined@assays$RNA@counts[which(rowSums(combined@assays$RNA@counts) != 0),]
-nrow(non_empty)
-
-results <- CytoTRACE(as.matrix(combined@assays$RNA@counts))
-plotCytoTRACE(results, emb = combined@reductions$umap@cell.embeddings[,1:2], outputDir = "/nv/hp10/ggruenhagen3/scratch/d_tooth/results/")
+obj <- epi
+results <- CytoTRACE(as.matrix(obj@assays$RNA@counts))
+plotCytoTRACE(results, emb = obj@reductions$umap@cell.embeddings[,1:2], outputDir = "/nv/hp10/ggruenhagen3/scratch/d_tooth/results/")
