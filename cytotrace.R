f8ea5d088ca10d814658018b6d943641353217e6
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-10 15:32:55 -0400

    Cytotrace

---
+++ cytotrace.R
@@ -0,0 +1,9 @@
+library("Seurat")
+library("Matrix")
+library("reticulate")
+library("cowplot")
+library("CytoTRACE")
+
+combined <- readRDS("/nv/hp10/ggruenhagen3/scratch/d_tooth/data/combined.rds")
+results <- CytoTRACE(as.matrix(combined@assays$RNA@counts))
+plotCytoTRACE(results, phenotype = as.matrix(combined@assays$RNA@counts), emb = combined@reductions$umap[,1:2], outputDir = "/nv/hp10/ggruenhagen3/scratch/d_tooth/results/")
